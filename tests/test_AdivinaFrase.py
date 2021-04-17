4fb629d501d9e3704061741a4c94b8748381a2a0
Javier De Juan Trujillo <tools@javierdejuan.es>
2021-04-17 11:25:50 +0200

    test: Adivina frase

--- tests/test_AdivinaFrase.py
+++ tests/test_AdivinaFrase.py
@@ -66,3 +66,35 @@ class AdivinaFrase(TestCase):
         juego_frase.adivinar_letra(frase_a_adivinar)
         juego_frase.adivinar_palabra(frase_a_adivinar)
         self.assertEqual('****r*r frase', juego_frase.mostrar_frase().strip())
+
+    @patch('src.JuegoFrase.obtener_string_de_usuario', return_value='adivino la frase')
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', return_value=4)
+    @patch('src.JuegoFrase.obtener_numero_de_usuario', return_value=2)
+    def test_no_adivina_frase(self, mock_frase_intentada, mock_longitud, mock_opcion_deseada):
+        frase_a_adivinar = "no adivino la frase"
+        juego_frase = JuegoFrase(frase_a_adivinar, 1)
+        self.assertFalse(juego_frase.adivinar_frase())
+
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', side_effect=[2, 9])
+    def test_no_adivino_longitud(self, mock_obtener_numero_usuario):
+        frase_a_adivinar = "adivino la frase"
+        juego_frase = JuegoFrase(frase_a_adivinar, 2)
+        self.assertFalse(juego_frase.adivinar_frase())
+
+    @patch('src.JuegoPalabra.obtener_string_de_usuario', side_effect=['q', 'test'])
+    @patch('src.JuegoFrase.obtener_string_de_usuario', return_value='adivino la frase')
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', return_value=4)
+    @patch('src.JuegoFrase.obtener_numero_de_usuario', side_effect=[0, 1, 2])
+    def test_no_adivina_nada(self, mock_strings_jp, mock_strings_jf, mock_numeros_jn, mock_numero_jf):
+        frase_a_adivinar = "no adivino la frase"
+        juego_frase = JuegoFrase(frase_a_adivinar, 2)
+        self.assertFalse(juego_frase.adivinar_frase())
+
+    @patch('src.JuegoPalabra.obtener_string_de_usuario', side_effect=['l', 'frase'])
+    @patch('src.JuegoFrase.obtener_string_de_usuario', side_effect=['casi adivino la frase', 'adivino la frase'])
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', return_value=3)
+    @patch('src.JuegoFrase.obtener_numero_de_usuario', side_effect=[0, 1, 2, 2])
+    def test_adivino_todo(self, mock_strings_jp, mock_strings_jf, mock_numeros_jn, mock_numero_jf):
+        frase_a_adivinar = "adivino la frase"
+        juego_frase = JuegoFrase(frase_a_adivinar, 2)
+        self.assertTrue(juego_frase.adivinar_frase())
