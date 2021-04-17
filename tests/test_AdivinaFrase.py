430ee1c2e95bcda7ff323ec7f4e8e80a144f536d
Javier De Juan Trujillo <tools@javierdejuan.es>
2021-04-17 11:24:46 +0200

    test: Adivina numero

--- tests/test_AdivinaFrase.py
+++ tests/test_AdivinaFrase.py
@@ -29,3 +29,13 @@ class AdivinaFrase(TestCase):
     def test_obtener_string_secreto_de_usuario_ok(self, getInputMock):
         self.assertEqual(obtener_string_de_usuario("Introduce una palabra", True), "test")
 
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', return_value=4)
+    def test_adivina_numero(self, getInputMock):
+        juego_numero = JuegoNumero(4, 4)
+        self.assertTrue(juego_numero.adivinar_numero())
+
+    @patch('src.JuegoNumero.obtener_numero_de_usuario', return_value=3)
+    def test_no_adivina_numero(self, getInputMock):
+        juego_numero = JuegoNumero(4, 2)
+        self.assertFalse(juego_numero.adivinar_numero())
+
